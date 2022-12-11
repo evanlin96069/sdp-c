870cc26e8a946840dc2a7159ba64e22f21d9bde3
evanlin96069 <72735402+evanlin96069@users.noreply.github.com>
2022-12-10 21:51:14 -0500

    Added icon.

--- compile.bat
+++ compile.bat
@@ -5,26 +5,39 @@ set lib_dir=lib
 set obj_dir=obj
 set src_dir=src
 
+set warnings=-Wall -Wextra -pedantic -Wno-unused-parameter -Wno-strict-aliasing
+set cflags=-I"include" %warnings% -std=c99
+
+set dbg=0
+
+if %dbg% == 1 (
+    set cflags=%cflags% -O0 -g3 -D_DEBUG
+) else (
+    set cflags=%cflags% -O3 -s
+)
+
 if not exist %bin_dir% mkdir %bin_dir%
 if not exist %lib_dir% mkdir %lib_dir%
 if not exist %obj_dir% mkdir %obj_dir%
 
 for /r %src_dir% %%f in (*.c) do (
   if /I not "%%~dpf"=="%src_dir%\utils\" (
-    gcc -c -o "%obj_dir%\%%~nf.o" -I"include" "%%f"
+    gcc -c -o "%obj_dir%\%%~nf.o" %cflags% "%%f"
   )
 )
 
 for /r %src_dir%\utils\ %%f in (*.c) do (
-  gcc -c -o "%obj_dir%\%%~nf.o" -I"include" "%%f"
+  gcc -c -o "%obj_dir%\%%~nf.o" %cflags% "%%f"
 )
 
 ar rcs %lib_dir%\libdemo.a %obj_dir%\*.o
-rd /s /q %obj_dir%
+windres "app\sdp.rc" -O coff --use-temp-file -o "%obj_dir%\sdp.res"
 
-gcc -I"include" -L"%lib_dir%" -o"%bin_dir%\sdp.exe" "app\sdp.c" "%lib_dir%\libdemo.a"
-gcc -I"include" -L"%lib_dir%" -o"%bin_dir%\bits.test.exe" "test\bits.test.c" "%lib_dir%\libdemo.a"
-gcc -I"include" -L"%lib_dir%" -o"%bin_dir%\demo.test.exe" "test\demo.test.c" "%lib_dir%\libdemo.a"
+gcc %cflags% -o"%bin_dir%\sdp.exe" "app\sdp.c" "%obj_dir%\sdp.res" "%lib_dir%\libdemo.a"
+gcc %cflags% -o"%bin_dir%\bits.test.exe" "test\bits.test.c" "%lib_dir%\libdemo.a"
+gcc %cflags% -o"%bin_dir%\demo.test.exe" "test\demo.test.c" "%lib_dir%\libdemo.a"
 
 %bin_dir%\bits.test.exe
 %bin_dir%\demo.test.exe
+
+rd /s /q %obj_dir%
--- compile.sh
+++ compile.sh
@@ -1,9 +1,19 @@
-#!/bin/sh
+#!/bin/sh -e
 
 bin_dir=bin
 lib_dir=lib
 obj_dir=obj
 src_dir=src
+
+warnings="-Wall -Wextra -pedantic -Wno-unused-parameter -Wno-strict-aliasing"
+cflags="-Iinclude $warnings -std=c99"
+
+dbg=0
+if [ $dbg = 1 ]; then
+	cflags="$cflags -g3 -D_DEBUG"
+else
+	cflags="$cflags -O3 -s"
+fi
 
 if [ ! -d "$bin_dir" ]; then
   mkdir $bin_dir
@@ -19,20 +29,20 @@ fi
 
 for file in $src_dir/*.c; do
   if [ "$file" != "$src_dir/utils/" ]; then
-    gcc -c -o "$obj_dir/$(basename "$file" .c).o" -I"include" "$file"
+    gcc -c -o "$obj_dir/$(basename "$file" .c).o" $cflags "$file"
   fi
 done
 
 for file in $src_dir/utils/*.c; do
-  gcc -c -o "$obj_dir/$(basename "$file" .c).o" -I"include" "$file"
+  gcc -c -o "$obj_dir/$(basename "$file" .c).o" $cflags "$file"
 done
 
 ar rcs $lib_dir/libdemo.a $obj_dir/*.o
 rm -rf obj
 
-gcc -I"include" -L"$lib_dir" -o"$bin_dir/sdp" "app/sdp.c" "$lib_dir/libdemo.a"
-gcc -I"include" -L"$lib_dir" -o"$bin_dir/bits.test" "test/bits.test.c" "$lib_dir/libdemo.a"
-gcc -I"include" -L"$lib_dir" -o"$bin_dir/demo.test" "test/demo.test.c" "$lib_dir/libdemo.a"
+gcc  $cflags -L"$lib_dir" -o"$bin_dir/sdp" "app/sdp.c" "$lib_dir/libdemo.a"
+gcc  $cflags -L"$lib_dir" -o"$bin_dir/bits.test" "test/bits.test.c" "$lib_dir/libdemo.a"
+gcc  $cflags -L"$lib_dir" -o"$bin_dir/demo.test" "test/demo.test.c" "$lib_dir/libdemo.a"
 
 $bin_dir/bits.test
 $bin_dir/demo.test
